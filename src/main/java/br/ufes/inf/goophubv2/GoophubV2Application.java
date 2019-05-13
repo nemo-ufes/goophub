0aad3b5cccfc1c2e9122ce3692be0637f1184f83
Gabriel Nogueira <gabrielgoncalvesnogueira@gmail.com>
2019-05-13 14:05:51 -0300

    Initial API implemented

---
+++ src/main/java/br/ufes/inf/goophubv2/GoophubV2Application.java
@@ -0,0 +1,15 @@
+package br.ufes.inf.goophubv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:applicationContext.xml")
+public class GoophubV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoophubV2Application.class, args);
+	}
+
+}
